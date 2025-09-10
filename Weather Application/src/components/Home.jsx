0927cd03b2a910d2dcf67b2b8af6a0bfc5f66181
Shikhar Srivastav <shikharsrivastav9450@gmail.com>
2025-09-10 23:29:06 +0530

    api

--- Weather Application/src/components/Home.jsx
+++ Weather Application/src/components/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
+  const [data, setData] = useState(null)
+
+  const api = "http://api.weatherapi.com/v1/current.json?key=1a89446531354e12af7173117251009&q=London&aqi=no"
+  // const api_api = "https://api.weatherapi.com/v1/current.json?key=1a89446531354e12af7173117251009&q=delhi&aqi=yes"
+
+   const FetchData = async() => {
+    const res = await fetch(api)
+    
+    const resdata = res.json() 
+
+    setData(resdata)
+
+    console.log("data ==== ", resdata.value.location);
+  }
+
   return (
-    <div>
-      Home Page
+    <div className='bg-black h-screen w-full text-gray-300'>
+      
+      <div className='bg-gray-500 max-h-screen  text-white mx-32 p-10 rounded-4xl'>
+               {data?.location?.name}
+      </div>
+
+          <button
+          className=''
+          onClick={FetchData}
+          >
+            getdata
+          </button>
     </div>
   )
 }
